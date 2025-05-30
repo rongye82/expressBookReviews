f9436c60a19da7562538305eed8ce519f3cd0b84
thomaskoh1982 <40250470+thomaskoh1982@users.noreply.github.com>
2025-05-30 14:52:57 +0800

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 });
 
 // Dynamic port for Vercel
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
